f3c510c81b7e8d7ba2e7dffc6f83cc2ae4d7d205
Mital B Topiwala <topiwal3@dh2020pc01.utm.utoronto.ca>
2019-01-17 18:47:08 -0500

    Added part of output that was forgotten

--- t02/pp_marks.sh
+++ t02/pp_marks.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-d="t01"
+atotal=0
+amaxtotal=0
+
+ttotal=0
+tmaxtotal=0
 
 
 
-for x in t01 t02 t03 t04 t05 t06 t07 t08 t09 t10 t11 a1 a2 a3 a4
+for x in a1 a2 a3 a4
 do
     
     if [ -f "$x/feedback.txt" ]
@@ -15,7 +19,9 @@ do
         IFS='/' read -ra grArray <<< "$grade" #creates an array with the character before and after the / 
         score=${grArray[0]}
         max=${grArray[1]}
-
+        
+        atotal=$total+${grArray[0]}
+        amaxtotal=$maxtotal+${grArray[1]}
         echo $x": "$score" / "$max
     fi
 
@@ -23,4 +29,31 @@ do
     then
         echo $x": - / -"
     fi
+
 done
+echo ""
+for x in t01 t02 t03 t04 t05 t06 t07 t08 t09 t10 t11
+do
+
+    if [ -f "$x/feedback.txt" ]
+    then
+
+        grade=`tail -n 1 $x/feedback.txt | tr -d [:blank:]` #removes the spaces from last
+
+        IFS='/' read -ra grArray <<< "$grade" #creates an array with the character before and after the / 
+        score=${grArray[0]}
+        max=${grArray[1]}
+
+        ttotal=$total+${grArray[0]}
+        tmaxtotal=$maxtotal+${grArray[1]}
+        echo $x": "$score" / "$max
+    fi
+
+    if [ ! -f "$x/feedback.txt" ]
+    then
+        echo $x": - / -"
+    fi
+done
+echo "" 
+echo "Assignment Total: "$atotal" / "$amaxtotal
+echo "Tutorial Total: "$ttotal" / "$tmaxtotal 
