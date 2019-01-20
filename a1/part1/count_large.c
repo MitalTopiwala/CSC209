fcb653baecc3413e05a2794baf384a247ad326fc
Mital B Topiwala <topiwal3@dh2020pc01.utm.utoronto.ca>
2019-01-20 18:28:54 -0500

    completed check_permission function

--- a1/part1/count_large.c
+++ a1/part1/count_large.c
@@ -3,7 +3,16 @@
 
 
 // TODO: Implement a helper named check_permissions that matches the prototype below.
-int check_permissions(char *, char *);
+int check_permissions(char * standard, char * tocheck) {
+    for (i=0;i<9;i++){
+        if (!(standard[i]=="-")){
+            if(!(standard[i]==tocheck[i])){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 
 
 int main(int argc, char** argv) {
@@ -13,8 +22,13 @@ int main(int argc, char** argv) {
     }
 
     // TODO: Process command line arguments.
-
+    if (argc == 2) {
+        //print number of files that are larger than argv[0][0] in size
+    }else{
+        //print numbers of files that are larger than argv[0][0] in size, and             have at least the permissions specified in argv[1][0]
+    
     // TODO Call check permissions and then print the returned value.
-
+         
+    }
     return 0;
-}
+
