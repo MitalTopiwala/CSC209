bf4f1ca3ffe9681f73d29c623d31ed9ecf3c722b
Mital B Topiwala <topiwal3@dh2020pc02.utm.utoronto.ca>
2019-01-24 14:55:40 -0500

    Created phone.c, but is not complete

---
+++ t03/phone.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int go =1;
+
+    while(go!=0){
+        char* s[10];
+        int num;
+        scanf("%s %d", s, &num);
+        if(num ==0){
+            //print string
+        }
+
+
+    }
+
+}
