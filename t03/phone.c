ee4f4c2cf8b0f439703dc0ba3299fa807f5a213b
Mital B Topiwala <topiwal3@dh2026pc01.utm.utoronto.ca>
2019-01-24 16:46:37 -0500

    completed phone.c

--- t03/phone.c
+++ t03/phone.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int go =1;
-
-    while(go!=0){
-        char* s[10];
-        int num;
-        scanf("%s %d", s, &num);
+    int num;
+    char s[10];
+        
+    while(scanf("%10s %d", s, &num) != EOF){
         if(num ==0){
-            //print string
+            printf("%s\n", s);
+
+        }else{
+            printf("%c\n",(int) s[num-1]);
+
         }
+   
 
 
     }
