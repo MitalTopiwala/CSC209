c6adcdc1d568b08ae0e0d5579f86c6bca9970faf
Mital B Topiwala <topiwal3@dh2026pc01.utm.utoronto.ca>
2019-01-24 17:31:09 -0500

    redid main function

--- a1/part1/count_large.c
+++ a1/part1/count_large.c
@@ -16,19 +16,41 @@ int check_permissions(char * standard, char * tocheck) {
 
 
 int main(int argc, char** argv) {
+
     if (!(argc == 2 || argc == 3)) {
         fprintf(stderr, "USAGE:\n\tcount_large size [permissions]\n");
         return 1;
     }
-    array=($(/bin/ls -l));
-    // TODO: Process command line arguments.
-    if (argc == 2) {
-        //print number of files that are larger than argv[0][0] in size
-    }else{
-        //print numbers of files that are larger than argv[0][0] in size, and             have at least the permissions specified in argv[1][0]
     
+    char perm[9];
+    int size;
+    char name[31];
+    while (scanf("%[9]s %*d %*s %*s %d %*d %*s %*s %s", perm, size, name ) {
+    }   
+    
+    // TODO: Process command line arguments.
+    int total =0;
+    if (argc == 2) {
+        //print number of files that are larger than argv[1] in size
+     
+        while(scanf("%*s %*d %*s %*s %d %*d %*s %*s %s",size,name) != EOM){
+            if (size > argv[1]){
+                total+=1;
+            }
+        }
+                
+
+    }else{
+        //print numbers of files that are larger than argv[1] in size, and             have at least the permissions specified in argv[2]
+        while(scanf("%[9]s %*d %*s %*s %d %*d %*s %*s %s",perm,size,name)!=EOM){
+
     // TODO Call check permissions and then print the returned value.
-           
+            if(size > argv[1] && check_permission(argv[2], perm)==0){
+                total+=1;
+            }
+        }
+               
     }
+    printf("%d", total);
     return 0;
 }
