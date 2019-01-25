c7c962ab170e4875acff5ac1bec639f479f7811b
Mital B Topiwala <topiwal3@dh2026pc01.utm.utoronto.ca>
2019-01-25 16:59:09 -0500

    completed populate_array function

--- a1/part2/sin_helpers.c
+++ a1/part2/sin_helpers.c
@@ -3,6 +3,14 @@
  * Convert a 9 digit int to a 9 element int array.
  */
 int populate_array(int sin, int *sin_array) {
+    if( sin%100000000 == 0 || sin%100000000 > 9){
+        return 1;
+    }
+    for(int i = 0; i<9;i++){
+        int last_digit = (sin/10 - sin%10)*10;
+        sin_array[8-i] = last_digit;
+        sin = sin%10;
+    }
     return 0;
 }
 
