cccf892837ba35725cfc59fa2706f3b6a712c4d4
Mital B Topiwala <topiwal3@dh2026pc01.utm.utoronto.ca>
2019-02-28 13:50:40 -0500

    updated parentcreates.c

--- t07/parentcreates.c
+++ t07/parentcreates.c
@@ -17,27 +17,25 @@ int main(int argc, char **argv) {
 	num_kids = strtol(argv[1], NULL, 10);
 
 	for (i = 0; i < num_kids; i++) {
-            for(int j = i; j<num_kids; j++){
+//            for(int j = i; j<num_kids; j++){
                 int ih = i;
-                int jh = j;
-		i = num_kids;
-                j = num_kids;
+//                int jh = j;
+		i = num_kids-1;
+//                j = num_kids;
                 n = fork();
                 if(n > 0){
                     i = ih;
-                    j = jh;
+//                    j = jh;
                 }
                 
-                //if (n == 0){
-                    
-                //}
+         
                 
 		if (n < 0) {
 			perror("fork");
 			exit(1);
 		}
  		printf("pid = %d, ppid = %d, i = %d\n", getpid(), getppid(), i);
-	    }
+	 //   }
         }
 
 	return 0;
