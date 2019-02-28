fdaa77851a9a01f401d09bf0374140fd45613d6b
Mital B Topiwala <topiwal3@dh2026pc01.utm.utoronto.ca>
2019-02-28 13:45:18 -0500

    completed tasks 1,2,3

--- t07/parentcreates.c
+++ t07/parentcreates.c
@@ -6,7 +6,7 @@
 int main(int argc, char **argv) {
 
 	int i;
-	int n;
+	int n = 0;
 	int num_kids;
 
 	if (argc != 2) {
@@ -17,13 +17,28 @@ int main(int argc, char **argv) {
 	num_kids = strtol(argv[1], NULL, 10);
 
 	for (i = 0; i < num_kids; i++) {
-		n = fork();
+            for(int j = i; j<num_kids; j++){
+                int ih = i;
+                int jh = j;
+		i = num_kids;
+                j = num_kids;
+                n = fork();
+                if(n > 0){
+                    i = ih;
+                    j = jh;
+                }
+                
+                //if (n == 0){
+                    
+                //}
+                
 		if (n < 0) {
 			perror("fork");
 			exit(1);
 		}
  		printf("pid = %d, ppid = %d, i = %d\n", getpid(), getppid(), i);
-	}
+	    }
+        }
 
 	return 0;
 }
