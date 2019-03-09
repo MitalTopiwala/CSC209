d9857164c3e96425df9dc245d43f6ed73a908685
Mital B Topiwala <topiwal3@dh2026pc02.utm.utoronto.ca>
2019-03-09 17:17:26 -0500

    created hepler function called primefactor in pfact.c

--- a3/pfact.c
+++ a3/pfact.c
@@ -18,5 +18,39 @@ int main(int argc, char *argv[]) {
     }
 
     // Your solution below ...
+    if(argc !=2){
+        fprintf(stderr, "Usage:\n\tpfact n\n");
+    }
+
+
+    
     return 0;
+}
+
+void primefact(int n){
+    //first create a pipe
+    int fd[2];
+    int pipe_ret = pid(fd);
+    if(pipe_ret == -1){//check for error
+        perror("pipe");
+        exit(1);
+    } 
+    //Now fork
+    int fork_ret = fork();
+    if(f_out == -1){//checl for error
+        perror("fork");
+        exit(1);
+    }
+    
+    if(fork_ret > 0){
+    //we are in the parent
+
+    }else{
+    //we are in the child
+
+
+    }
+
+
+
 }
