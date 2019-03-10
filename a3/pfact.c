1bc9b68d96b435905703083416246a9049e6e841
Mital B Topiwala <topiwal3@dh2026pc02.utm.utoronto.ca>
2019-03-10 00:47:36 -0500

    added more lines to primefact() function

--- a3/pfact.c
+++ a3/pfact.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Your solution below ...
+    //chcek that correct arguements are given
     if(argc !=2){
         fprintf(stderr, "Usage:\n\tpfact n\n");
     }
@@ -31,22 +32,36 @@ void primefact(int n){
     //first create a pipe
     int fd[2];
     int pipe_ret = pid(fd);
-    if(pipe_ret == -1){//check for error
+    if(pipe_ret == -1){//check for error in creating pipe
         perror("pipe");
         exit(1);
     } 
     //Now fork
     int fork_ret = fork();
-    if(f_out == -1){//checl for error
+    if(f_out == -1){//checl for error in creating fork
         perror("fork");
         exit(1);
     }
     
     if(fork_ret > 0){
     //we are in the parent
+    close(fd[0]);
+    
+    //write values from 1 to n into pipe
+    for(int i = 2; i<=n; i++){
+        int write_ret  = write(fd[1], &i, sizeof(int));
+        if(write_ret == -1){
+            //what do i do? raise an error?then do i exit?
+            //perror("writing to pipe");
+            //exit(1);
+        }
+
+
+    }
 
     }else{
     //we are in the child
+    //call the make_stage() function?
 
 
     }
