31fffb0b245cadadd69e4820a752a5e825440700
Mital B Topiwala <topiwal3@dh2026pc02.utm.utoronto.ca>
2019-03-11 10:11:04 -0400

    created outline for make_stage function

--- a3/pipeline_stage.c
+++ a3/pipeline_stage.c
@@ -4,4 +4,35 @@
 
 pid_t make_stage(int n, int read_fd, int **fds) {
     // TODO: Complete
+    //we are in the child process
+    int *actual_fds = *fds;
+    close(fd[1]);
+
+    int fd2[2];
+
+    int pipe_ret = pipe(fd2);
+    if(pipe_ret == -1){
+        //error
+        exit(1);//?
+    }
+
+    int fork_ret = fork();
+    if(fork_ret == -1){
+        //perror
+        exit(1);
+    }
+    if (fork_ret >0){
+    //we are in the childs (perent)
+
+
+
+
+    }else{
+    //we are in the childs child
+
+
+
+
+   }
+    
 }
