b0122c161d41962b1330aa608987fc8d3f519c04
Mital B Topiwala <topiwal3@dh2026pc02.utm.utoronto.ca>
2019-03-11 00:17:21 -0400

    completed filter.c

--- a3/filter.c
+++ a3/filter.c
@@ -4,5 +4,13 @@
 
 int filter(int n, int readfd, int writefd) {
     // TODO: Complete
+    int value;
+    while (int readfd_ret = read(readfd, &value, sizeof(int)) != -1){
+     
+
+        if((n%value) != 0){
+            int writefd_ret = write(writefd, &value, sizeof(int));
+        }
+    }
 }
 
